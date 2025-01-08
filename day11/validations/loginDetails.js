5f0ff7b4447aeed216c46738f3220bc283c4ce87
suyanthapa <146444078+suyanthapa@users.noreply.github.com>
2025-01-08 15:57:13 +0545

    Updated submodule content

--- day11/validations/loginDetails.js
+++ day11/validations/loginDetails.js
@@ -5,9 +5,9 @@ const detailsValidation = {
   body: Joi.object().keys({
 
     name: Joi.string().min(3).required(),
-    password: Joi.number().min(8).required(),
+    password: Joi.string().min(8).required(),
     address: Joi.string().min(3).required(),
-     phone: Joi.number().min(3).required(),
+     phone: Joi.number().min(10000000).required(),
      email: Joi.string().min(3).required()
 
   })
